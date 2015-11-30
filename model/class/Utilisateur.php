e9fffe2d308532da9766d301556abb256b0fe1cf
Alexis <Alexis@Alexis-PC-Port>
2015-11-30 11:38:34 +0100

    Ajout des class et DAL de l'ensemble du projet (sauf Singleton)

---
+++ model/class/Utilisateur.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Utilisateur
+ * Possède un tableau des groupe auxquels appartient cette utilisateur, avec le role qu'il possde dans chacun de ces groupe
+ * @author Alexis
+ */
+class Utilisateur {
+    //put your code here
+}
