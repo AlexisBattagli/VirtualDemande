4760c1e9bd6ad877a00dc29b394548d78824b129
Alexis <Alexis@Alexis-PC-Port>
2015-12-03 17:44:44 +0100

    Ajout des getter et setter pour la class Distrib_Alias

--- model/class/Distrib_Alias.php
+++ model/class/Distrib_Alias.php
@@ -1,25 +1,23 @@
 <?php
 
-
 /**
  * Description of Distrib_Alias
  *
  * @author Alexis
  */
-
 class Distrib_Alias {
-    
-/* 
-   ==============================
-   ========= ATTRIBUTS ==========
-   ============================== 
-*/ 
+    /*
+      ==============================
+      ========= ATTRIBUTS ==========
+      ==============================
+     */
     /*
      * Id d'un Distrib_Alias dans la table Distrib_Alias
      * @var int 
      */
+
     private $id;
-    
+
     /*
      * Distrib de la distrib alias
      * @var Distrib
@@ -30,17 +28,113 @@ class Distrib_Alias {
      * nom_complet d'une distrib alias
      * @var string
      */
-    private $nom_complet;
-    
+    private $nomComplet;
+
     /*
      * pseudo de la distri_alias
      * @var string
      */
     private $pseudo;
-    
+
     /*
      * commentaire de la distrib alias
      * @vat string
      */
     private $commentaire;
+
+    /*
+      ==============================
+      ======= GETTER/SETTER ========
+      ==============================
+     */
+
+    //id
+    public function setId($id)
+    {
+        if (is_int($id))
+        {
+            $this->id = $id;
+        }
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    //Ditrib
+    public function setDistrib($distrib)
+    {
+        if (is_string($distrib))
+        {
+            $distrib = (int) $distrib;
+            $this->distrib = DistribDAL::findById($distrib);
+        }
+        else if (is_int($distrib))
+        {
+            $this->distrib = DistribDAL::findById($distrib);
+        }
+        else if (is_a($distrib, "Distrib"))
+        {
+            $this->distrib = $distrib;
+        }
+    }
+
+    public function getDistrib()
+    {
+        $distrib = null;
+        if (is_int($this->distrib))
+        {
+            $distrib = DistribDAL::findById($this->distrib);
+            $this->distrib = $distrib;
+        }
+        else if (is_a($this->distrib, "Distrib"))
+        {
+            $distrib = $this->distrib;
+        }
+        return $distrib;
+    }
+
+    //nom_complet
+    public function setNomComplet($nomComplet)
+    {
+        if (is_string($nomComplet))
+        {
+            $this->nomComplet = $nomComplet;
+        }
+    }
+
+    public function getNomComplet()
+    {
+        return $this->nomComplet;
+    }
+
+    //pseudo
+    public function setPseudo($pseudo)
+    {
+        if (is_string($pseudo))
+        {
+            $this->pseudo = $pseudo;
+        }
+    }
+
+    public function getPseudo()
+    {
+        $this->pseudo;
+    }
+
+    //commentaire
+    public function setCommentaire($commentaire)
+    {
+        if (is_string($commentaire))
+        {
+            $this->commentaire = $commentaire;
+        }
+    }
+
+    public function getCommentaire()
+    {
+        $this->commentaire;
+    }
+
 }
