9e434bdd4447a195a7c92d8b07d388a4d3c2f9cd
aueoe$ <Utilisateur@Aurelie>
2015-12-27 18:46:25 +0100

    Class Distrib réalisée.

--- model/class/Distrib.php
+++ model/class/Distrib.php
@@ -12,5 +12,152 @@
  * @author Alexis
  */
 class Distrib {
-    //put your code here
+    /*
+      ==============================
+      ========= ATTRIBUTS ==========
+      ==============================
+     */
+    /*
+     * Id d'un Distrib dans la table Distrib
+     * @var int 
+     */
+
+    private $id;
+    
+    /*
+     * Nom d'un Distrib dans la table Distrib
+     * @var int 
+    */
+
+    private $nom;
+    
+    /*
+     * Archi d'un Distrib dans la table Distrib
+     * @var string
+     */
+    private $archi;
+    
+    /*
+     * Version d'un Distrib dans la table Distrib
+     * @var string
+     */
+    private $version;
+    
+    /*
+     * IHM d'un Distrib dans la table Distrib
+     * @var string
+     */
+    private $ihm;
+    
+    /*
+      ==============================
+      ======== CONSTRUCTEUR ========
+      ==============================
+     *
+
+      /*
+     * Constructeur par défaut de Distrib
+     */
+
+    public function Distrib_Alias(
+    $id = -1, $nom = -1, $archi = "Aucun archi pour cette Distrib", $version = "Cette Distrib n'a pas de version", $ihm = "Cette Distrib n'a pas de IHM"
+    )
+    {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->archi = $archi;
+        $this->version = $version;
+        $this->ihm = $ihm;
+    }
+    
+    /*
+      ==============================
+      ========== METHODES ==========
+      ==============================
+     */
+
+    protected function hydrate($dataSet)
+    {
+        $this->id = $dataSet['id'];
+        $this->nom = $dataSet['nom'];
+        $this->archi = $dataSet['archi'];
+        $this->version = $dataSet['version'];
+        $this->ihm = $dataSet['ihm'];
+    }
+    
+    /*
+      ==============================
+      ======= GETTER/SETTER ========
+      ==============================
+     */
+
+    //id
+    public function setId($id)
+    {
+        if (is_int($id))
+        {
+            $this->id = $id;
+        }
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    //nom
+    public function setNom($nom)
+    {
+        if (is_int($nom))
+        {
+            $this->nom = $nom;
+        }
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    
+    //archi
+    public function setArchi($archi)
+    {
+        if (is_string($archi))
+        {
+            $this->archi = $archi;
+        }
+    }
+
+    public function getArchi()
+    {
+        return $this->archi;
+    }
+    
+    //version
+    public function setVersion($version)
+    {
+        if (is_string($version))
+        {
+            $this->version = $version;
+        }
+    }
+
+    public function getVersion()
+    {
+        return $this->version;
+    }
+    
+    //ihm
+    public function setIhm($ihm)
+    {
+        if (is_string($ihm))
+        {
+            $this->ihm = $ihm;
+        }
+    }
+
+    public function getIhm()
+    {
+        return $this->ihm;
+    }
 }
