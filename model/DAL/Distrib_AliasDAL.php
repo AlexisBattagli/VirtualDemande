df204f0b446c446de1dd6095e140d051d0a64020
Alexis <Alexis@Alexis-PC-Port.home>
2016-01-03 09:36:41 +0100

    Création de la DAL de Distrib_Alias Ajout de la méthode findById()

--- model/DAL/Distrib_AliasDAL.php
+++ model/DAL/Distrib_AliasDAL.php
@@ -1,16 +1,44 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
  * Description of Distrib_AliasDAL
  *
  * @author Alexis
  */
+
+/*
+ * IMPORT
+ */
+require_once('BaseSingleton.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/class/Distrib_Alias.php');
+
 class Distrib_AliasDAL {
-    //put your code here
+    
+    /*
+     * Retourne l'alias d'une distrib correspondant à l'id donnée
+     * 
+     * @param int $id
+     * @return Distrib_Alias
+     */
+    public static function findById($id)
+    {
+        $data = BaseSingleton::select('SELECT distrib_alias.id as id, '
+                        . 'distrib_alias.Distrib_id as Distrib_id, '
+                        . 'distrib_alias.nom_complet as nom_complet, '
+                        . 'distrib_alias.pseudo as pseudo, '
+                        . 'distrib_alias.commentaire as commentaire '
+                        . ' FROM distrib_alias'
+                        . ' WHERE distrib_alias.id = ?', array('i', &$id));
+        $distribAlias = new Distrib_Alias();
+        if (sizeof($data) > 0)
+        {
+            $distribAlias->hydrate($data[0]);
+        }
+        else
+        {
+            $distribAlias = null;
+        }
+        return $distribAlias;
+    }
+
 }
