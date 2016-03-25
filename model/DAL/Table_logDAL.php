f770e18029d7263e368e604eac949904976c5709
Utilisateur <Utilisateur@Aurelie>
2016-03-25 23:58:26 +0100

    Création de la class Table_log et DAL Table_logDAL

---
+++ model/DAL/Table_logDAL.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Table_logDAL
+ *
+ * @author Utilisateur
+ */
+class Table_logDAL {
+    //put your code here
+}
