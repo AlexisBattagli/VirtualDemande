650406e1d0a91c9645a1f4c5c5272975cadbcbe6
hundrex <kiruahxh32@yahoo.fr>
2016-05-25 20:06:02 +0200

    Commit pour la création des pages et liens how does it work/register/server dispo/ what is it et modif d'index.php en fonction

--- index.php
+++ index.php
@@ -18,6 +18,8 @@
         <!-- <link rel="icon" type="image/png" href="./view/document/picture/favicon.png" />   -->
     </head>
     <body>
+        
+        <!-- Nav bar -->
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
@@ -28,16 +30,16 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">EVOLVE</a>
+                    <a class="navbar-brand" href="?page=home">EVOLVE</a>
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">What is it ? <span class="sr-only">(current)</span></a></li>
-                        <li><a href="#">How does it work ?</a></li>
-                        <li><a href="#">Server disponibility</a></li>
-                        <li><a href="#">Register</a></li>
+                        <li class="active"><a href="?page=what_is_it">What is it ? <span class="sr-only">(current)</span></a></li>
+                        <li><a href="?page=how_does_it_work">How does it work ?</a></li>
+                        <li><a href="?page=server_disponibility">Server disponibility</a></li>
+                        <li><a href="?page=register">Register</a></li>
                     </ul>
 
                     <ul class="nav navbar-nav navbar-right">
@@ -55,6 +57,25 @@
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
         </nav>
+        
+        
+        <!-- <div id="left-column" class="col-lg-2"></div>   Décalage à droite de deux colonnes. A garder ?-->
+        
+        <!-- Page to show -->
+        <div id="content" class="col-lg-8">
+            <?php
+            $page_to_require = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_URL);
+            if ($page_to_require !== null)
+            {
+                require_once './view/phtml/' . $page_to_require . '.php';
+            }
+            else
+            {
+                require_once './view/phtml/home.php';
+            }
+            ?>
+        </div>
+        <div id="right-column" class="col-lg-2"></div>
     </body>
 </html>
 
