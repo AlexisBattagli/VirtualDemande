0bc04ac6abea7e00f76cd1cd4fc773effa3b7519
Aurelie <hundrix@ubuntuTest1>
2016-06-03 15:48:39 +0200

    - Modification pour l'ajout d'un guacamole_user : 	- Problème d'insertion ou mise à jour car le password_salt est merdeux!!

--- model/DAL/Guacamole_UserDAL.php
+++ model/DAL/Guacamole_UserDAL.php
@@ -22,7 +22,7 @@ class Guacamole_UserDAL {
     public static function findByDefault()
     {
         $id=1;
-        $data = BaseSingleton::select('SELECT guacamole_user.user_id as user_id, '
+        $data = BaseSingletonGuacamole::select('SELECT guacamole_user.user_id as user_id, '
                         . 'guacamole_user.username as username, '
                         . 'guacamole_user.password_hash as password_hash, '
                         . 'guacamole_user.password_salt as password_salt, '
@@ -56,7 +56,7 @@ class Guacamole_UserDAL {
     
     public static function findById($id)
     {
-        $data = BaseSingleton::select('SELECT guacamole_user.user_id as user_id, '
+        $data = BaseSingletonGuacamole::select('SELECT guacamole_user.user_id as user_id, '
                         . 'guacamole_user.username as username, '
                         . 'guacamole_user.password_hash as password_hash, '
                         . 'guacamole_user.password_salt as password_salt, '
@@ -90,7 +90,7 @@ class Guacamole_UserDAL {
     {
         $mesGuacamoleUsers = array();
 
-        $data = BaseSingleton::select('SELECT guacamole_user.user_id as user_id, '
+        $data = BaseSingletonGuacamole::select('SELECT guacamole_user.user_id as user_id, '
                         . 'guacamole_user.username as username, '
                         . 'guacamole_user.password_hash as password_hash, '
                         . 'guacamole_user.password_salt as password_salt, '
@@ -124,7 +124,7 @@ class Guacamole_UserDAL {
      */
      public static function findByUsername($username)
     {
-        $data = BaseSingleton::select('SELECT guacamole_user.user_id as user_id, '
+        $data = BaseSingletonGuacamole::select('SELECT guacamole_user.user_id as user_id, '
                         . 'guacamole_user.username as username, '
                         . 'guacamole_user.password_hash as password_hash, '
                         . 'guacamole_user.password_salt as password_salt, '
@@ -174,13 +174,29 @@ class Guacamole_UserDAL {
         $validUntil = $guacamoleUser->getValidUntil(); //string
         $timezone = $guacamoleUser->getTimezone(); //string
         $userId = $guacamoleUser->getUserId(); //int
-      
+        $newguacamoleUser=null;
+
         if ($userId < 0)
         {
-            $sql = 'SET @salt = UNHEX(SHA2(UUID(), 256));'
-                    . ' INSERT INTO guacamole_user (username, password_hash, password_salt, disabled, expired, access_window_start, access_window_end, valid_from, valid_until, timezone) '
-                    . ' VALUES (?,UNHEX(SHA2(CONCAT(?, HEX(@salt)), 256)), HEX(@salt)), 256)),@salt,?,?,?,?,?,?,?) ';
-
+            
+            /*
+             * SET @salt = UNHEX(SHA2(UUID(), 256));
+INSERT INTO guacamole_user (username, password_hash, password_salt, disabled, expired, access_window_start, access_window_end, valid_from, valid_until, timezone)
+VALUES ("pass",UNHEX(SHA2(CONCAT("pass", HEX(UNHEX(SHA2(UUID(), 256)))), 256)),UNHEX(SHA2(UUID(), 256)),0,0,NULL,NULL,NULL,NULL,NULL)
+             * //Le problème est que l'exécution doit se faire l'un après l'autre car sinon si on les fait séparer il garde pas en mémoire @salt !!!
+             
+            sql = ' INSERT INTO guacamole_user (username, password_hash, password_salt, disabled, expired, access_window_start, access_window_end, valid_from, valid_until, timezone) '
+                  . ' VALUES (?,UNHEX(SHA2(CONCAT(?, HEX(UNHEX(SHA2(UUID(), 256)))), 256)),UNHEX(SHA2(UUID(), 256)),?,?,?,?,?,?,?) ';
+            //Ne s'insère pas car password_salt n'est pas le bon... : la bonne requête serait : SET @salt = UNHEX(SHA2(UUID(), 256));
+//INSERT INTO guacamole_user (username, password_hash, password_salt, disabled, expired, access_window_start, access_window_end, valid_from, valid_until, timezone)
+//VALUES ("pass",@UNHEX(SHA2(CONCAT("pass", HEX(UNHEX(SHA2(UUID(), 256)))), 256)),@salt,0,0,NULL,NULL,NULL,NULL,NULL)
+             
+            /*$sql ='INSERT INTO guacamole_user (username, password_hash, password_salt, disabled, expired, access_window_start, access_window_end, valid_from, valid_until, timezone)'
+            .'VALUES (?, x’sha256(?)’, 256), null, 0, 0, ?, ?, ?, ?, ?)';
+            */
+            
+            $sql = ' INSERT INTO guacamole_user (username, password_hash, password_salt, disabled, expired, access_window_start, access_window_end, valid_from, valid_until, timezone) '
+                  . ' VALUES (?,UNHEX(SHA2(CONCAT(?, HEX(UNHEX(SHA2(UUID(), 256)))), 256)),UNHEX(SHA2(UUID(), 256)),?,?,?,?,?,?,?) ';
             //Prépare les info concernant les type de champs
             $params = array('ssiisssss',
                 &$username,
@@ -193,32 +209,37 @@ class Guacamole_UserDAL {
                 &$validUntil,
                 &$timezone
             );
+            
+            $idInsert = BaseSingletonGuacamole::insertOrEdit($sql, $params);
+            $newguacamoleUser=new Guacamole_User();
+            $newguacamoleUser=Guacamole_UserDAL::findById($idInsert);
+            if ($newguacamoleUser != null)
+            {
+                //echo "pas là";
+                $newguacamoleUser->setPasswordHash($passwordHash);
+                $idInsert = Guacamole_UserDAL::insertOnDuplicate($newguacamoleUser);echo "OK";
+            }
         }
         else
-        {
-            /*
-             SET @salt = UNHEX(SHA2(UUID(), 256));
-UPDATE guacamole_user 
-                    SET username = "a",
-                    password_hash = UNHEX(SHA2(CONCAT("mypass", HEX(@salt)), 256)),
-                    password_salt = @salt,
+        { echo "Ici";
+        
+        /*Bonne requete :
+         * UPDATE guacamole_user
+                    SET username = "aa",
+                    password_hash = UNHEX(SHA2(CONCAT("aa", HEX(password_salt)), 256)),
                     disabled = 0, 
                     expired = 0, 
-                    access_window_start = 0000/00/00, 
-                    access_window_end = 0000/00/00, 
+                    access_window_start = NULL,
+                    access_window_end = NULL, 
                     valid_from = NULL, 
-                    valid_until = NULL,
-                    timezone = "OK"
-                    WHERE user_id = 10;
-             */
-            
-            
-            
-            $sql = 'SET @salt = UNHEX(SHA2(UUID(), 256));'
-                    . ' UPDATE guacamole_user '
+                    valid_until = NULL, 
+                    timezone = NULL 
+                    WHERE user_id = 76;
+        
+              
+            $sql = ' UPDATE guacamole_user '
                     . 'SET username = ?, '
-                    . 'password_hash = UNHEX(SHA2(CONCAT(?, HEX(@salt)), 256)), '
-                    . 'password_salt = @salt, '
+                    . 'password_hash = UNHEX(SHA2(CONCAT(?, HEX(guacamole_user.password_salt)), 256)), '
                     . 'disabled = ?, '
                     . 'expired = ?, '
                     . 'access_window_start = ?, '
@@ -227,7 +248,19 @@ UPDATE guacamole_user
                     . 'valid_until = ?, '
                     . 'timezone = ? '
                     . 'WHERE user_id = ? ';
-
+*/
+        $sql ='UPDATE guacamole_user '
+                    .'SET username = ?, '
+                    .'password_hash = UNHEX(SHA2(CONCAT(?, HEX(password_salt)), 256)), '
+                    .'disabled = ?, '
+                    .'expired = ?, '
+                    .'access_window_start = ?, '
+                    .'access_window_end = ?, '
+                    .'valid_from = ?, '
+                    .'valid_until = ?, '
+                    .'timezone = ? '
+                    .'WHERE user_id = ?;';
+        
             //Prépare les info concernant les type de champs
             $params = array('ssiisssssi',
                 &$username,
@@ -241,11 +274,13 @@ UPDATE guacamole_user
                 &$timezone,
                 &$userId
             );
+            echo $userId;
+            
         }
 
         //Exec la requête
         $idInsert = BaseSingletonGuacamole::insertOrEdit($sql, $params);
-
+        
         return $idInsert;
     }
     
