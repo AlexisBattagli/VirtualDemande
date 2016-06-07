6a40ae586fd15d3ac15a3026cde3247b940b3c7d
hundrex <kiruahxh32@yahoo.fr>
2016-06-07 22:14:56 +0200

    1ers changements pour la nav bar en fonction de connect/non connect

--- index.php
+++ index.php
@@ -2,23 +2,33 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/UtilisateurDAL.php');
 session_start();
+$user =null;
 $pseudo = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'passwd', FILTER_SANITIZE_STRING);
-if ($pseudo !== null && $password !== null)
-{
-    $user = UserDAL::connection($pseudo, $password);
-    if ($user)
-    {
+
+//$user = new Utilisateur();
+//$user->setLogin($pseudo);
+//$user->setPassword($password);
+//$user->setId(15);
+//$user->setRole('lapin');
+
+//User connexion
+if ($pseudo !== null && $password !== null) {
+//    $user = UtilisateurDAL::connection($pseudo, $password);
+    if ($user) {
         $_SESSION['user'] = $user->getId();
         $_SESSION['role'] = $user->getRole();
+
         setcookie("user_id", $_SESSION['user']);
         setcookie("user_role", $_SESSION['role']);
-    }
-    else
-    {
+    } else {
         $_SESSION['user'] = false;
     }
 }
+
+//    debug
+//var_dump($user);
+//die();
 ?>
 
 <html>
@@ -43,61 +53,103 @@ if ($pseudo !== null && $password !== null)
         <!-- <link rel="icon" type="image/png" href="./view/document/picture/favicon.png" />   -->
     </head>
     <body>
-        
-        <!-- Nav bar -->
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="?page=home">EVOLVE</a>
-                </div>
 
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    
-                    
-                    <ul class="nav navbar-nav">
-                        <li id="what_is_it"><a href="?page=what_is_it">What is it ? <span class="sr-only">(current)</span></a></li>
-                        <li id="how_does_it_work"><a href="?page=how_does_it_work">How does it work ?</a></li>
-                        <li id="register"><a href="?page=register">Register</a></li>
-                    </ul>
-                    <p class="navbar-text">Il reste X comptes disponibles</p>
+        <?php if (!(isset($user) && !empty($user) && $user !== false)): ?>
+            <!-- Nav bar for unconnected user -->
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="?page=home">EVOLVE unplug</a>
+                    </div>
 
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <form class="navbar-form" role="search">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Username">
-                                    <input type="text" class="form-control" placeholder="Password">
-                                </div>
-                                <button type="submit" class="btn btn-default">Log In</button>
-                            </form>
-                        </li>
-                        <li><a href="#">Sign up</a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
-        
-        
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+                        <ul class="nav navbar-nav">
+                            <li id="what_is_it"><a href="?page=what_is_it">What is it ? <span class="sr-only">(current)</span></a></li>
+                            <li id="how_does_it_work"><a href="?page=how_does_it_work">How does it work ?</a></li>
+                            <li id="register"><a href="?page=register">Register</a></li>
+                        </ul>
+
+                        <!--A implémenter correctement-->
+                        <p class="navbar-text">Il reste X comptes disponibles</p>    
+
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <form class="navbar-form" action="index.php" method="post">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control"name="login" placeholder="Username">
+                                        <input type="password" class="form-control" name="passwd" placeholder="Password">
+                                    </div>
+                                    <button type="submit" class="btn btn-default">Log In</button>
+                                </form>
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        <?php else :?>
+            <!-- Nav bar for connected user -->
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="?page=home">EVOLVE</a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+                        <ul class="nav navbar-nav">
+                            <li id="what_is_it"><a href="?page=what_is_it">What is it ? <span class="sr-only">(current)</span></a></li>
+                            <li id="how_does_it_work"><a href="?page=how_does_it_work">How does it work ?</a></li>
+                            <li id="register"><a href="?page=register">Register</a></li>
+                        </ul>
+
+                        <!--A implémenter correctement-->
+                        <p class="navbar-text">Il reste X comptes disponibles</p>    
+
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <form class="navbar-form" action="index.php" method="post">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control"name="login" placeholder="Username">
+                                        <input type="password" class="form-control" name="passwd" placeholder="Password">
+                                    </div>
+                                    <button type="submit" class="btn btn-default">Log In</button>
+                                </form>
+                            </li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+        <?php endif; ?>
+
+            
         <!-- <div id="left-column" class="col-lg-2"></div>   Décalage à droite de deux colonnes. A garder ?-->
-        
+
         <!-- Page to show -->
         <div id="content" class="col-lg-8">
             <?php
             $page_to_require = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_URL);
-            if ($page_to_require !== null)
-            {
+            if ($page_to_require !== null) {
                 require_once './view/phtml/' . $page_to_require . '.php';
-            }
-            else
-            {
+            } else {
                 require_once './view/phtml/home.php';
             }
             ?>
