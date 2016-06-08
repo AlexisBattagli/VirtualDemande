52d2e3b82964d5f7c18d6d925228e333ee9662a9
Utilisateur <Utilisateur@Aurelie>
2016-06-08 20:41:44 +0200

    - Modification du controller Create_User

--- controller/pages/Create_User.php
+++ controller/pages/Create_User.php
@@ -13,151 +13,164 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/Utilisateur_
 require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/LimitantDAL.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/Table_logDAL.php');
 
-//Création d'un Utilisateur par défaut
-$newUtilisateur=new Utilisateur();
+//Définition du message renvoyé
+$message="error";
 
-//=====Vérification de ce qui est renvoyé par le formulaire
-$validLogin = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_STRING);
+//Checker de où il vient
 
-if ($validLogin != null)
+$validPage = filter_input(INPUT_POST, 'page', FILTER_SANITIZE_STRING);
+
+if($validPage == "register.php")
 {
-    $newUtilisateur->setLogin($validLogin);
-    //echo "OK pour Login : ".$newUtilisateur->getLogin();
-}
+    //Création d'un Utilisateur par défaut
+    $newUtilisateur=new Utilisateur();
 
-$validNom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
-if ($validNom != null)
-{
-    $newUtilisateur->setNom($validNom);
-    //echo "OK pour Nom :".$newUtilisateur->getNom();
-}
+    //=====Vérification de ce qui est renvoyé par le formulaire
+    $validLogin = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_STRING);
 
-$validPrenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_STRING);
-if ($validPrenom != null)
-{
-    $newUtilisateur->setPrenom($validPrenom);
-    //echo "OK pour Prenom :".$newUtilisateur->getPrenom();
-}
-
-$validEmail = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
-if ($validEmail != null)
-{
-    $newUtilisateur->setMail($validEmail);
-    //echo "OK pour Mail :".$newUtilisateur->getMail();
-}
-
-$validDate = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING);
-$date = DateTime::createFromFormat('d/m/Y', $validDate);
-$validDateFormat=$date->format('Y/m/d');
-if ($validDateFormat != null)
-{
-    $newUtilisateur->setDateNaissance($validDateFormat);
-    //echo "OK pour Date de Naissance:".$newUtilisateur->getDateNaissance();
-}
-
-$validPassword = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-if ($validPassword != null)
-{
-    $newUtilisateur->setPassword($validPassword);
-    //echo "OK pour Passwd:".$newUtilisateur->getPassword();
-}
-
-//nb_VM + dateCreation + roleId à générer en plus
-
-$newUtilisateur->setNbVm("0");
-//echo "OK pour NbVm:".$newUtilisateur->getNbVm();
-
-$newDateCreation=date("Y/m/d");
-$newUtilisateur->setDateCreation($newDateCreation);
-//echo "OK pour DateCréation:".$newUtilisateur->getDateCreation();
-
-$newUtilisateur->setRole("2");
-//echo "OK pour Role_id:".$newUtilisateur->getRole()->getId();
-
-//====Vérification de doublons==== - OK
-if (UtilisateurDAL::isUnique($validLogin,$validEmail) == null)
-{
-//=====Insertion=====/ - OK
-    $validInsertUtilisateur = UtilisateurDAL::insertOnDuplicate($newUtilisateur);
-
-    if ($validInsertUtilisateur != null)
+    if ($validLogin != null)
     {
-        //echo "Ajout de l'utilisateur reussi dans la base DBVirtDemande ! (id:" . $validInsertUtilisateur . ")";
-        //Création d'un guacamole_user
+        $newUtilisateur->setLogin($validLogin);
+        //echo "OK pour Login : ".$newUtilisateur->getLogin();
+    }
 
-        $newUserGuacamole=new Guacamole_User();
+    $validNom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_STRING);
+    if ($validNom != null)
+    {
+        $newUtilisateur->setNom($validNom);
+        //echo "OK pour Nom :".$newUtilisateur->getNom();
+    }
 
-        //=====Vérification de ce qui est renvoyé par le formulaire
-        $validUserName = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_STRING);
-        if ($validUserName != null)
+    $validPrenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_STRING);
+    if ($validPrenom != null)
+    {
+        $newUtilisateur->setPrenom($validPrenom);
+        //echo "OK pour Prenom :".$newUtilisateur->getPrenom();
+    }
+
+    $validEmail = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
+    if ($validEmail != null)
+    {
+        $newUtilisateur->setMail($validEmail);
+        //echo "OK pour Mail :".$newUtilisateur->getMail();
+    }
+
+    $validDate = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_STRING);
+    $date = DateTime::createFromFormat('d/m/Y', $validDate);
+    $validDateFormat=$date->format('Y/m/d');
+    if ($validDateFormat != null)
+    {
+        $newUtilisateur->setDateNaissance($validDateFormat);
+        //echo "OK pour Date de Naissance:".$newUtilisateur->getDateNaissance();
+    }
+
+    $validPassword = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    if ($validPassword != null)
+    {
+        $newUtilisateur->setPassword($validPassword);
+        //echo "OK pour Passwd:".$newUtilisateur->getPassword();
+    }
+
+    //nb_VM + dateCreation + roleId à générer en plus
+
+    $newUtilisateur->setNbVm("0");
+    //echo "OK pour NbVm:".$newUtilisateur->getNbVm();
+
+    $newDateCreation=date("Y/m/d");
+    $newUtilisateur->setDateCreation($newDateCreation);
+    //echo "OK pour DateCréation:".$newUtilisateur->getDateCreation();
+
+    $newUtilisateur->setRole("2");
+    //echo "OK pour Role_id:".$newUtilisateur->getRole()->getId();
+
+    //====Vérification de doublons==== - OK
+    if (UtilisateurDAL::isUnique($validLogin,$validEmail) == null)
+    {
+    //=====Insertion=====/ - OK
+        $validInsertUtilisateur = UtilisateurDAL::insertOnDuplicate($newUtilisateur);
+
+        if ($validInsertUtilisateur != null)
         {
-            $newUserGuacamole->setUserName($validUserName);
-            //echo "OK pour Username : ".$newUserGuacamole->getUsername();
-        }
+            //echo "Ajout de l'utilisateur reussi dans la base DBVirtDemande ! (id:" . $validInsertUtilisateur . ")";
+            //Création d'un guacamole_user
 
-        $validPassword = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+            $newUserGuacamole=new Guacamole_User();
 
-        if ($validPassword != null)
-        {
-            $newUserGuacamole->setPasswordHash($validPassword);
-            //echo "OK pour PasswdHash:".$newUserGuacamole->getPasswordHash();
-        }
-
-        //A rajouter : expired et disabled : mettre à 0
-
-            $newUserGuacamole->setDisabled(0);
-            //echo "OK pour Disabled:".$newUserGuacamole->getDisabled();
-
-            $newUserGuacamole->setExpired(0);
-            //echo "OK pour Expired:".$newUserGuacamole->getExpired();
-
-            //Les $accessWindowStart, $accessWindowEnd doivent être à null sinon ils ne pourront pas accéder à n'importe quelle heure sur leurs machines
-            //$validFrom=null, $validUntil=null, pareils
-
-        //echo "Valider";
-
-        //====Vérification de doublons====
-        if (Guacamole_UserDAL::findByUsername($validUserName) == null)
-        {
-        //=====Insertion=====/ - OK
-
-            $validInsertUser = Guacamole_UserDAL::insertOnDuplicate($newUserGuacamole);
-
-            if ($validInsertUser != null)
+            //=====Vérification de ce qui est renvoyé par le formulaire
+            $validUserName = filter_input(INPUT_POST, 'pseudo', FILTER_SANITIZE_STRING);
+            if ($validUserName != null)
             {
-                //echo "Ajout de l'utilisateur reussi dans la base guacamole_db! (id:" . $validInsertUser . ")";
-                //Paramètres des permissions de l'utilisateur
-                $guacamoleUserPermission=new Guacamole_User_Permission();
-                $guacamoleUserPermission->setUser($validInsertUser);
-                $guacamoleUserPermission->setAffectedUser($validInsertUser);
-                $guacamoleUserPermission->setPermission("READ");
-                $valid=Guacamole_User_PermissionDAL::insertOnDuplicate($guacamoleUserPermission);
-                $guacamoleUserPermission->setPermission("UPDATE");
-                $valid=Guacamole_User_PermissionDAL::insertOnDuplicate($guacamoleUserPermission);
-                $guacamoleUserPermission->setPermission("DELETE");
-                $valid=Guacamole_User_PermissionDAL::insertOnDuplicate($guacamoleUserPermission);
-                $guacamoleUserPermission->setPermission("ADMINISTER");
-                $valid=Guacamole_User_PermissionDAL::insertOnDuplicate($guacamoleUserPermission);
+                $newUserGuacamole->setUserName($validUserName);
+                //echo "OK pour Username : ".$newUserGuacamole->getUsername();
+            }
+
+            $validPassword = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
+            if ($validPassword != null)
+            {
+                $newUserGuacamole->setPasswordHash($validPassword);
+                //echo "OK pour PasswdHash:".$newUserGuacamole->getPasswordHash();
+            }
+
+            //A rajouter : expired et disabled : mettre à 0
+
+                $newUserGuacamole->setDisabled(0);
+                //echo "OK pour Disabled:".$newUserGuacamole->getDisabled();
+
+                $newUserGuacamole->setExpired(0);
+                //echo "OK pour Expired:".$newUserGuacamole->getExpired();
+
+                //Les $accessWindowStart, $accessWindowEnd doivent être à null sinon ils ne pourront pas accéder à n'importe quelle heure sur leurs machines
+                //$validFrom=null, $validUntil=null, pareils
+
+            //echo "Valider";
+
+            //====Vérification de doublons====
+            if (Guacamole_UserDAL::findByUsername($validUserName) == null)
+            {
+            //=====Insertion=====/ - OK
+
+                $validInsertUser = Guacamole_UserDAL::insertOnDuplicate($newUserGuacamole);
+
+                if ($validInsertUser != null)
+                {
+                    //echo "Ajout de l'utilisateur reussi dans la base guacamole_db! (id:" . $validInsertUser . ")";
+                    //Paramètres des permissions de l'utilisateur
+                    $guacamoleUserPermission=new Guacamole_User_Permission();
+                    $guacamoleUserPermission->setUser($validInsertUser);
+                    $guacamoleUserPermission->setAffectedUser($validInsertUser);
+                    $guacamoleUserPermission->setPermission("READ");
+                    $valid=Guacamole_User_PermissionDAL::insertOnDuplicate($guacamoleUserPermission);
+                    $guacamoleUserPermission->setPermission("UPDATE");
+                    $valid=Guacamole_User_PermissionDAL::insertOnDuplicate($guacamoleUserPermission);
+                    $guacamoleUserPermission->setPermission("DELETE");
+                    $valid=Guacamole_User_PermissionDAL::insertOnDuplicate($guacamoleUserPermission);
+                    $guacamoleUserPermission->setPermission("ADMINISTER");
+                    $valid=Guacamole_User_PermissionDAL::insertOnDuplicate($guacamoleUserPermission);
+                    
+                    $message="ok";
+                }
+                else
+                {
+                    //echo "insert echec...";
+                }
             }
             else
             {
-                //echo "insert echec...";
-            }
+                //echo "Erreur, l'utilisateur que vous voulez ajouter existe déjà...";
+            }    
         }
         else
         {
-            //echo "Erreur, l'utilisateur que vous voulez ajouter existe déjà...";
-        }    
+            //echo "insert echec...";
+        }
     }
     else
     {
-        //echo "insert echec...";
+        //echo "Erreur, l'utilisateur que vous voulez ajouter existe...";
     }
-}
-else
-{
-    //echo "Erreur, l'utilisateur que vous voulez ajouter existe...";
+    
 }
 
 //Renvoie à la page précédante
-    echo "<meta http-equiv='refresh' content='1; url=".$_SERVER["HTTP_REFERER"]. "' />";
+    echo "<meta http-equiv='refresh' content='1; url=".$_SERVER["HTTP_REFERER"].'?message='.$message. "' />";
