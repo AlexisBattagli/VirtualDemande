9a0ae4f0c023a66b2514764024a20750827551df
aurelie$ <Utilisateur@Aurelie>
2016-06-08 16:57:59 +0200

    - Création des fichiers pour accueillir les controllers concernant l'administration de distrib, distribalias, ram, process, stockage

---
+++ controller/pages/Update_Distrib.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Update_Distrib
+ *
+ * @author Utilisateur
+ */
+class Update_Distrib {
+    //put your code here
+}
---
+++ controller/pages/Update_Ram.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Update_Ram
+ *
+ * @author Utilisateur
+ */
+class Update_Ram {
+    //put your code here
+}
