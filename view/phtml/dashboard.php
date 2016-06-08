6c0da28e01e39bc7572507861fee86be3fd4f721
hundrex <kiruahxh32@yahoo.fr>
2016-06-09 01:01:15 +0200

    Affichage containers utilisateur dans dashboard

--- view/phtml/dashboard.php
+++ view/phtml/dashboard.php
@@ -1,8 +1,33 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/MachineDAL.php');
+$rows = MachineDAL::findByUser($_COOKIE["user_id"]);
+?>
+<html>
+    <body>
+        <div>
+            <h2><span class="label label-primary">Your containers</span></h2>
+            <table class = "table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>OS</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($rows as $containers) {
+                        echo "<tr><td>";
+                        echo $containers[0];
+                        echo "</td><td>";
+                        echo $containers[1];
+                        echo "</td><td>";
+                        echo $containers[2];
+                        echo "</td></tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
