c7bd1e467bc605935291cfddead744f268fda4b4
AlexisBattagli <sasukalex-kun@hotmail.fr>
2016-06-08 02:44:51 -0700

    Update fill_distribs_v1.sql
    AJout des distribs et distrib_alis en bdd , testé ok !

--- scripts/fill_distribs_v1.sql
+++ scripts/fill_distribs_v1.sql
@@ -2,49 +2,49 @@
 INSERT INTO distrib (id,nom,archi,version, ihm, visible)
 VALUE (2, 'debian', 'amd64', 'sid', 'yes', true);
 INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
-VALUE (2, 'Debian Sid Graphical 64bits', 'Debian', 'Distrib Debian with release Sid in 64 bits and graphical version.');
+VALUE (2, 'Debian Sid Graphical 64bits', 'Debian', 'Distrib Debian with release Sid in 64 bits and graphical version.', true);
 
 -- Debian_sid_i386
 INSERT INTO distrib (id,nom,archi,version, ihm, visible)
 VALUE (3, 'debian', 'i386', 'sid', 'yes', true);
 INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
-VALUE (3, 'Debian Sid Graphical 32bits', 'Debian', 'Distrib Debian with release Sid in 32 bits and graphical version.');
+VALUE (3, 'Debian Sid Graphical 32bits', 'Debian', 'Distrib Debian with release Sid in 32 bits and graphical version.', true);
 
 -- Debian_jessie_amd64
 INSERT INTO distrib (id,nom,archi,version, ihm, visible)
 VALUE (4, 'debian', 'amd64', 'jessie', 'yes', true);
 INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
-VALUE (4, 'Debian Jessie Graphical 64bits', 'Debian', 'Distrib Debian with release Jessie in 64 bits and graphical version.');
+VALUE (4, 'Debian Jessie Graphical 64bits', 'Debian', 'Distrib Debian with release Jessie in 64 bits and graphical version.', true);
 
 -- Debian_jessie_i386
 INSERT INTO distrib (id,nom,archi,version, ihm, visible)
 VALUE (5, 'debian', 'i386', 'jessie', 'yes', true);
 INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
-VALUE (5, 'Debian Jessie Graphical 32bits', 'Debian', 'Distrib Debian with release Jessie in 32 bits and graphical version.');
+VALUE (5, 'Debian Jessie Graphical 32bits', 'Debian', 'Distrib Debian with release Jessie in 32 bits and graphical version.', true);
 
 -- Debian_sid_amd64 
 INSERT INTO distrib (id,nom,archi,version, ihm, visible)
-VALUE (2, 'debian', 'amd64', 'sid', 'no', true);
+VALUE (6, 'debian', 'amd64', 'sid', 'no', true);
 INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
-VALUE (2, 'Debian Sid Console 64bits', 'Debian', 'Distrib Debian with release Sid in 64 bits and console version.');
+VALUE (6, 'Debian Sid Console 64bits', 'Debian', 'Distrib Debian with release Sid in 64 bits and console version.', true);
 
 -- Debian_sid_i386
 INSERT INTO distrib (id,nom,archi,version, ihm, visible)
-VALUE (3, 'debian', 'i386', 'sid', 'no', true);
+VALUE (7, 'debian', 'i386', 'sid', 'no', true);
 INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
-VALUE (3, 'Debian Sid Console 32bits', 'Debian', 'Distrib Debian with release Sid in 32 bits and console version.');
+VALUE (7, 'Debian Sid Console 32bits', 'Debian', 'Distrib Debian with release Sid in 32 bits and console version.', true);
 
 -- Debian_jessie_amd64
 INSERT INTO distrib (id,nom,archi,version, ihm, visible)
-VALUE (4, 'debian', 'amd64', 'jessie', 'no', true);
+VALUE (8, 'debian', 'amd64', 'jessie', 'no', true);
 INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
-VALUE (4, 'Debian Jessie Console 64bits', 'Debian', 'Distrib Debian with release Jessie in 64 bits and console version.');
+VALUE (8, 'Debian Jessie Console 64bits', 'Debian', 'Distrib Debian with release Jessie in 64 bits and console version.', true);
 
 -- Debian_jessie_i386
 INSERT INTO distrib (id,nom,archi,version, ihm, visible)
-VALUE (5, 'debian', 'i386', 'jessie', 'no', true);
+VALUE (9, 'debian', 'i386', 'jessie', 'no', true);
 INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
-VALUE (5, 'Debian Jessie Console 32bits', 'Debian', 'Distrib Debian with release Jessie in 32 bits and console version.');
+VALUE (9, 'Debian Jessie Console 32bits', 'Debian', 'Distrib Debian with release Jessie in 32 bits and console version.', true);
 
 -- CentOS_6_amd64
 INSERT INTO distrib (id,nom,archi,version, ihm, visible)
