06cc14d28c4c2d1b6fcfc622fc616305969b3bc5
AlexisBattagli <battagli.alexis@hotmail.fr>
2016-06-08 11:30:13 +0200

    AJout v0 de fill_distribs

---
+++ scripts/fill_distribs_v1.sql
@@ -0,0 +1,98 @@
+-- Debian_sid_amd64 
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE (2, 'debian', 'amd64', 'sid', 'yes', true);
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE (2, 'Debian Sid Graphical 64bits', 'Debian', 'Distrib Debian with release Sid in 64 bits and graphical version.');
+
+-- Debian_sid_i386
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE (3, 'debian', 'i386', 'sid', 'yes', true);
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE (3, 'Debian Sid Graphical 32bits', 'Debian', 'Distrib Debian with release Sid in 32 bits and graphical version.');
+
+-- Debian_jessie_amd64
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE (4, 'debian', 'amd64', 'jessie', 'yes', true);
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE (4, 'Debian Jessie Graphical 64bits', 'Debian', 'Distrib Debian with release Jessie in 64 bits and graphical version.');
+
+-- Debian_jessie_i386
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE (5, 'debian', 'i386', 'jessie', 'yes', true);
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE (5, 'Debian Jessie Graphical 32bits', 'Debian', 'Distrib Debian with release Jessie in 32 bits and graphical version.');
+
+-- Debian_sid_amd64 
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE (2, 'debian', 'amd64', 'sid', 'no', true);
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE (2, 'Debian Sid Console 64bits', 'Debian', 'Distrib Debian with release Sid in 64 bits and console version.');
+
+-- Debian_sid_i386
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE (3, 'debian', 'i386', 'sid', 'no', true);
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE (3, 'Debian Sid Console 32bits', 'Debian', 'Distrib Debian with release Sid in 32 bits and console version.');
+
+-- Debian_jessie_amd64
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE (4, 'debian', 'amd64', 'jessie', 'no', true);
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE (4, 'Debian Jessie Console 64bits', 'Debian', 'Distrib Debian with release Jessie in 64 bits and console version.');
+
+-- Debian_jessie_i386
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE (5, 'debian', 'i386', 'jessie', 'no', true);
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE (5, 'Debian Jessie Console 32bits', 'Debian', 'Distrib Debian with release Jessie in 32 bits and console version.');
+
+-- CentOS_6_amd64
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE ();
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE ();
+
+-- CentOS_6_i386
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE ();
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE ();
+
+-- CentOS_7_amd64
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE ();
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE ();
+
+-- CentOS_7_i386
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE ();
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE ();
+
+-- Ubuntu_wily_amd64
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE ();
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE ();
+
+-- Ubuntu_wily_i386
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE ();
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE ();
+
+-- Ubuntu_xenial_amd64
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE ();
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE ();
+
+-- Ubuntu_xenial_i386
+INSERT INTO distrib (id,nom,archi,version, ihm, visible)
+VALUE ();
+INSERT INTO distrib_alias (distrib_id,nom_complet,pseudo,commentaire,visible)
+VALUE ();
+
+
+
