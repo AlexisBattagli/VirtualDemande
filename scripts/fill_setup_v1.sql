795ab58f91985cb4cfcafa335235e247df1eee3d
AlexisBattagli <sasukalex-kun@hotmail.fr>
2016-06-08 04:53:30 -0700

    Update fill_setup_v1.sql
    suppression du remplissage de la table limitant qui est déjà rempli par le script d'init

--- scripts/fill_setup_v1.sql
+++ scripts/fill_setup_v1.sql
@@ -1,7 +1,3 @@
--- Remplissage de la table limitant
-INSERT INTO limitant (nb_user_max, nb_vm_user)
-VALUE (6,2);
-
 -- Remplissage de la table role
 INSERT INTO role (nom_role, description)
 VALUE ('registered','Standard account of EVOLVE Web site.');
