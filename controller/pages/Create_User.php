b7ffd00554419ac0dc3eaf02776cdec35476b398
hundrix <hundrix@ubuntuTest1>
2016-06-08 23:21:00 +0200

    - Correction de Create_User

--- controller/pages/Create_User.php
+++ controller/pages/Create_User.php
@@ -173,4 +173,4 @@ if($validPage == "register.php")
 }
 
 //Renvoie à la page précédante
-    echo "<meta http-equiv='refresh' content='1; url=".$_SERVER["HTTP_REFERER"].'?page='.$validPage.'&message='.$message. "' />";
+    echo "<meta http-equiv='refresh' content='1; url=".$_SERVER["HTTP_REFERER"].'&message='.$message. "' />";
