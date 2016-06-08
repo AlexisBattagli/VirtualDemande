b6fe374af94646dbbd99e226facab4a07f5b8e09
AlexisBattagli <battagli.alexis@hotmail.fr>
2016-06-08 13:39:39 +0200

    AJout du script pour remplir la conf hardware des container en DB

---
+++ scripts/fill_hardware_v1.sql
@@ -0,0 +1,31 @@
+-- Remplissage de la table RAM
+INSERT INTO ram (valeur,visible)
+VALUE (512, true);
+INSERT INTO ram (valeur,visible)
+VALUE (1024, true);
+INSERT INTO ram (valeur,visible)
+VALUE (2048, true);
+
+-- Remplissage de la table CPU
+INSERT INTO cpu (nb_coeur,visible)
+VALUE (512, true);
+INSERT INTO cpu (nb_coeur,visible)
+VALUE (1024, true);
+INSERT INTO cpu (nb_coeur,visible)
+VALUE (2048, true);
+
+-- Remplissage de la table STOCKAGE
+INSERT INTO stockage (valeur,visible)
+VALUE (6, true);
+INSERT INTO stockage (valeur,visible)
+VALUE (7, true);
+INSERT INTO stockage (valeur,visible)
+VALUE (8, true);
+INSERT INTO stockage (valeur,visible)
+VALUE (9, true);
+INSERT INTO stockage (valeur,visible)
+VALUE (10, true);
+INSERT INTO stockage (valeur,visible)
+VALUE (11, true);
+
+
