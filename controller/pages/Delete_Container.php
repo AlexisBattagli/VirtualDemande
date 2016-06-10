44848e86c2f976bea1d6ae52fc736aa4b0c98ce4
Utilisateur <Utilisateur@Aurelie>
2016-06-10 12:44:34 +0200

    - Modif de DeleteContainer

--- controller/pages/Delete_Container.php
+++ controller/pages/Delete_Container.php
@@ -5,6 +5,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/UtilisateurDAL.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/MachineDAL.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/Groupe_has_MachineDAL.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/Table_logDAL.php');
 
 // guaca
 require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/Guacamole_ConnectionDAL.php');
@@ -13,14 +14,17 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/Guacamole_Co
 
 //Script pour supprimer le container voulu
 
+//Définition d'un objet Table_log pour faire des insert de log
+$newLog = new Table_log();
+
 //Définition du message renvoyé
 $message="error";
 
 //Checker de où il vient
 $validPage = filter_input(INPUT_POST, 'page', FILTER_SANITIZE_STRING);
 
-if($validPage == "DeleteContainer")
-{
+if($validPage == "manage_containers.php")
+{ 
     //=====Vérification de ce qui est renvoyé par le formulaire
     $validIdMachine = filter_input(INPUT_POST, 'idMachine', FILTER_SANITIZE_STRING);
 
@@ -29,10 +33,18 @@ if($validPage == "DeleteContainer")
 
     if($machine != null)
     {
+        //Récupérer le login de l'utilisateur
+        $loginUtilisateur=$machine->getUtilisateur()->getLogin();
+        
         //Récupérer le nom de la machine
         $nomMachine=$machine->getNom();
         //echo "Le nom de la machine à supprimer est".$nomMachine;
+        $newLog->setLevel("INFO");
+        $newLog->setLoginUtilisateur($loginUtilisateur);
+        $newLog->setMsg("Le nom de la machine à supprimer est".$nomMachine);
+        $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
         
+ /*       
         //=====Appel Web Service pour remove le container sur le serveur de virt=====/
         try
         {
@@ -56,45 +68,145 @@ if($validPage == "DeleteContainer")
             $validTableLog = Table_logDAL::insertOnDuplicate($newLog);
             exit();
         }
-
+*/
+        $result=0;
         if ($result == "0")
         {
             //Récupérer l'id de connexion
-            $connection=Guacamole_ConnectionDAL::findByNom($nomMachine);
+            $connection=Guacamole_ConnectionDAL::findByNom($nomMachine); 
             $connectionId=$connection->getConnectionId();
+            
+            $newLog->setLevel("INFO");
+            $newLog->setLoginUtilisateur($loginUtilisateur);
+            $newLog->setMsg("Début de la suppression de la connection n°".$connectionId);
+            $validTableLog = Table_logDAL::insertOnDuplicate($newLog);
             //echo "Début de la suppression de la connection n°$connectionId";
 
             //Supprimer les élements connection_parameter
             $validDeletePermission=Guacamole_Connection_ParameterDAL::deleteConnection($connectionId);
-            //echo "L'ensemble des parametres de connection pour la connection n° $connectionId ont bien été supprimés."; 
-
+            echo $connectionId;
+            if(is_null(Guacamole_Connection_ParameterDAL::findByConnection($connectionId)))
+            {
+                $newLog->setLevel("INFO");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("L'ensemble des parametres de connection pour la connection n° $connectionId ont bien été supprimés.");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "L'ensemble des parametres de connection pour la connection n° $connectionId ont bien été supprimés."; 
+            }
+            else
+            {
+                $newLog->setLevel("ERROR");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("L'ensemble des parametres de connection pour la connection n° $connectionId n'ont pas bien été supprimés.");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "L'ensemble des parametres de connection pour la connection n° $connectionId ont bien été supprimés."; 
+                exit();
+            }
+            
             //Supprimer les élements connection_permission
             $validDeleteParameter=Guacamole_Connection_PermissionDAL::deleteConnection($connectionId);
-            //echo "L'ensemble des permissions de connection pour la connection n° $connectionId ont bien été supprimés.";      
+            if(is_null(Guacamole_Connection_PermissionDAL::findByConnection($connectionId)))
+            {
+                $newLog->setLevel("INFO");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("L'ensemble des permissions de connection pour la connection n° $connectionId ont bien été supprimés.");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "L'ensemble des permissions de connection pour la connection n° $connectionId ont bien été supprimés.";      
+            }
+            else
+            {
+                $newLog->setLevel("ERROR");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("L'ensemble des permissions de connection pour la connection n° $connectionId n'ont pas bien été supprimés.");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "L'ensemble des permissions de connection pour la connection n° $connectionId ont bien été supprimés.";   
+                exit();
+            }
+            
+            
 
             //Supprimer le container dans la base guacamole
             $validDeleteConnection=Guacamole_ConnectionDAL::delete($connectionId);
-            //echo "La connection n°$connectionId a bien été supprimé.";  
+            if(is_null(Guacamole_ConnectionDAL::findById($id)))
+            {
+                $newLog->setLevel("INFO");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("La connection n°$connectionId a bien été supprimé.");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "La connection n°$connectionId a bien été supprimé."; 
+            }
+            else
+            {
+                $newLog->setLevel("ERROR");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("La connection n°$connectionId n'a pas bien été supprimé.");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "La connection n°$connectionId a bien été supprimé."; 
+                exit();
+            }
+             
 
             //Suppprimer les partages de cette machine
             $validDeletePartage=Groupe_has_MachineDAL::deleteMachine($validIdMachine);
-            //echo "La machine $nomMachine a bien été enlever des groupe dans le(s)quel(s) elle était partagée."; 
+            if(is_null(Groupe_has_MachineDAL::findByMachine($validIdMachine)))
+            {
+                $newLog->setLevel("INFO");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("La machine $nomMachine a bien été enlever des groupe dans le(s)quel(s) elle était partagée.");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "La machine $nomMachine a bien été enlever des groupe dans le(s)quel(s) elle était partagée."; 
+            }
+            else
+            {
+                $newLog->setLevel("INFO");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("La machine $nomMachine n'a pas bien été enlever des groupe dans le(s)quel(s) elle était partagée.");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "La machine $nomMachine a bien été enlever des groupe dans le(s)quel(s) elle était partagée."; 
+                exit();
+            }
+            
             
             //Trouve l'user de la machine et décrémente de 1 son nombre de Container
             $owner = $machine->getUtilisateur();
             $owner->setNbVm($owner->getNbVm() - 1); 
-            //!! ajouter insert !!
-            // echo "Le quota de l'utilisateur $owner->getPseudo() est maintenant à $owner->getNbVm()";
+            $validUser=UtilisateurDAL::insertOnDuplicate($owner);
+            $newLog->setLevel("INFO");
+            $newLog->setLoginUtilisateur($loginUtilisateur);
+            $newLog->setMsg("Le quota de l'utilisateur $owner->getPseudo() est maintenant à $owner->getNbVm()");
+            $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+            //echo "Le quota de l'utilisateur $owner->getPseudo() est maintenant à $owner->getNbVm()";
             
             //Supprimer le container dans la base DBVirtDemand
             $validDeleteMachine=MachineDAL::delete($validIdMachine);
-            //echo "La machine $nomMachine d'id $validIdMachine appartenant à l'utilisateur $owner->getPseudo() a bien été supprimée !";
+            if(is_null(MachineDAL::findById($id)))
+            {
+                $newLog->setLevel("INFO");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("La machine $nomMachine d'id $validIdMachine appartenant à l'utilisateur $owner->getPseudo() a bien été supprimée !");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "La machine $nomMachine d'id $validIdMachine appartenant à l'utilisateur $owner->getPseudo() a bien été supprimée !";
+            }
+            else
+            {
+                $newLog->setLevel("ERROR");
+                $newLog->setLoginUtilisateur($loginUtilisateur);
+                $newLog->setMsg("La machine $nomMachine d'id $validIdMachine appartenant à l'utilisateur $owner->getPseudo() n'a pas bien été supprimée !");
+                $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
+                //echo "La machine $nomMachine d'id $validIdMachine appartenant à l'utilisateur $owner->getPseudo() a bien été supprimée !";
+                exit();
+            }
         
             $message="ok";
         }
         else
         {
+            $newLog->setLevel("ERROR");
+            $newLog->setLoginUtilisateur($loginUtilisateur);
+            $newLog->setMsg("Erreur lors de la suppression du container nommé $nomMachine");
+            $validTableLog = Table_logDAL::insertOnDuplicate($newLog);     
             echo "Erreur lors de la suppression du container nommé ". $nomMachine; //TODO log
+            exit();
         }
     }
 }
