446b17b02729255e67ec91e4b9318f1a3be7dfee
hundrex <kiruahxh32@yahoo.fr>
2016-06-12 18:20:49 +0200

    Affichage des infos user dans profile.php

--- view/phtml/profile.php
+++ view/phtml/profile.php
@@ -1,8 +1,39 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/UtilisateurDAL.php');
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$userInformations = UtilisateurDAL::findById($_COOKIE["user_id"]);
+
+//echo "<pre>";
+//var_dump($userInformations);
+//echo "</pre>";
+?>
+
+<html>
+    <body>
+        <!--Name-->
+        <div>
+            <h2><b>Name : </b> <?=$userInformations->getPrenom() ?> </h2>
+        </div>
+        <!--Surname-->
+        <div>
+            <h2><b>Surname : </b> <?=$userInformations->getNom() ?> </h2>
+        </div>
+        <!--Login-->
+        <div>
+            <h2><b>Login : </b> <?=$userInformations->getLogin() ?> </h2>
+        </div>
+        <!--Email-->
+        <div>
+            <h2><b>E-mail : </b> <?=$userInformations->getMail() ?> </h2>
+        </div>
+        <!--Birth date-->
+        <div>
+            <h2><b>Birth date : </b> <?=$userInformations->getDateNaissance() ?> </h2>
+        </div>
+        <!--Account creation date-->
+        <div>
+            <h2><b>Account creation date : </b> <?=$userInformations->getDateCreation() ?> </h2>
+        </div>
+    </body>
+</html>
 
