fcdb6f943e99fa504f113345faea2fd86072a31a
hundrex <kiruahxh32@yahoo.fr>
2016-06-14 17:54:52 +0200

    ajout redirection si non root sur pages admin

--- view/phtml/forms_administration.php
+++ view/phtml/forms_administration.php
@@ -8,14 +8,11 @@ $OSList = Distrib_AliasDAL::findAll();
 $CPUList = CpuDAL::findAll();
 $RAMList = RamDAL::findAll();
 $HDDList = StockageDAL::findAll();
-
-//echo "<pre>";
-//var_dump($OSList);
-//echo "</pre>";
 ?>
 
 <html>
     <body>
+        <?php if ((isset($_SESSION['user_id']) && !empty($_SESSION['user_id']) && $_SESSION['user_id'] !== false) && $_SESSION["role_id"] == 3) : ?>
         <h2><span class="label label-warning">Visible items configuration - Check the items you want to be visible for the user</span></h2>
         <!--OS settings-->
         <form action="./controller/pages/Update_Distrib_Alias.php" method="post" >
@@ -105,5 +102,10 @@ $HDDList = StockageDAL::findAll();
                 <button type="submit" class="btn btn-default">Update</button>
             </div>
         </form>
+        <?php else: ?>
+            <script type="text/javascript">
+                window.location.href = "./?page=home"
+            </script>
+        <?php endif; ?>
     </body>
 </html>
--- view/phtml/logs.php
+++ view/phtml/logs.php
@@ -1,38 +1,39 @@
-<?php 
+<?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/VirtualDemande/model/DAL/Table_logDAL.php');
 
 $all_logs = Table_logDAL::findAll();
-
 ?>
 
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Admin EVOLVE: Logs</title>
-    </head>
     <body>
-        <div>
-            <h2><span class="label label-primary">Logs EVOLVE</span></h2>
-            <table class ="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th>Datetime</th>
-                        <th>Level</th>
-                        <th>User</th>
-                        <th>Message</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($all_logs as $log) : ?>
-                    <tr>
-                        <td><?= $log->getDateTime() ?></td>
-                        <td><?= $log->getLevel() ?></td>
-                        <td><?= $log->getLoginUtilisateur() ?></td>
-                        <td><?= $log->getMsg() ?></td>
-                    </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
+        <?php if ((isset($_SESSION['user_id']) && !empty($_SESSION['user_id']) && $_SESSION['user_id'] !== false) && $_SESSION["role_id"] == 3) : ?>
+            <div>
+                <h2><span class="label label-primary">Logs EVOLVE</span></h2>
+                <table class ="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>Datetime</th>
+                            <th>Level</th>
+                            <th>User</th>
+                            <th>Message</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($all_logs as $log) : ?>
+                            <tr>
+                                <td><?= $log->getDateTime() ?></td>
+                                <td><?= $log->getLevel() ?></td>
+                                <td><?= $log->getLoginUtilisateur() ?></td>
+                                <td><?= $log->getMsg() ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php else: ?>
+            <script type="text/javascript">
+                window.location.href = "./?page=home"
+            </script>
+        <?php endif; ?>
     </body>
 </html>
