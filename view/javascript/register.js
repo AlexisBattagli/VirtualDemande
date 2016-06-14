e0c503f9a3591b3a168904e887c6fb1d0704554d
hundrex <kiruahxh32@yahoo.fr>
2016-06-14 17:59:00 +0200

    Déport du script datepicker dans un js

---
+++ view/javascript/register.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#date-register').datetimepicker({
+        todayBtn: "true",
+        format: "dd/mm/yyyy",
+        autoclose: "true",
+        pickerPosition: "bottom-left",
+        minView: "month",
+        language: "fr"
+    });
+});
--- view/phtml/register.php
+++ view/phtml/register.php
@@ -8,11 +8,10 @@ $message = filter_input(INPUT_GET, 'message', FILTER_SANITIZE_STRING);
 
 <html>
     <body>
-        <head>
-            <link rel="stylesheet" href="./view/library/bootstrap/css/bootstrap-datetimepicker.css">
-            <script type="text/javascript" src="./view/library/bootstrap/js/bootstrap-datetimepicker.min.js"></script>
-            <script type="text/javascript" src="../view/library/bootstrap/js/bootstrap-datetimepicker.fr.js"></script>
-        </head>
+        <script src="./view/javascript/register.js"></script>
+        <link rel="stylesheet" href="./view/library/bootstrap/css/bootstrap-datetimepicker.css">
+        <script type="text/javascript" src="./view/library/bootstrap/js/bootstrap-datetimepicker.min.js"></script>
+        <script type="text/javascript" src="./view/library/bootstrap/js/bootstrap-datetimepicker.fr.js"></script>
         <div>
             <?php if ($message === 'ok'): ?> 
                 <div class="alert alert-success" role="alert">
@@ -33,59 +32,46 @@ $message = filter_input(INPUT_GET, 'message', FILTER_SANITIZE_STRING);
         <div>
             <?php if ($accountNumber > 0) : ?>
 
-            <form action="./controller/pages/Create_User.php" method="post" >
+                <form action="./controller/pages/Create_User.php" method="post" >
 
-                <div class="form-group">
-                    <input name="page" type="hidden" class="form-control" value ="register.php">
-                </div>
-
-                <div class="form-group">
-                    <label for="nameRegister">Name</label>
-                    <input name="prenom" type="name" class="form-control" id="nameRegister" placeholder="Name">
-                </div>
-                <div class="form-group">
-                    <label for="surnameRegister">Surname</label>
-                    <input name="nom" type="surname" class="form-control" id="surnameRegister" placeholder="Surname">
-                </div>
-                <div class="form-group">
-                    <label for="usernameRegister">Username</label>
-                    <input name="pseudo" type="username" class="form-control" id="usernameRegister" placeholder="Username">
-                </div>
-                <div class="form-group">
-                    <label for="emailRegister">Email address</label>
-                    <input name="email" type="email" class="form-control" id="emailRegister" placeholder="Email">
-                </div>
-                <div class="form-group">
-                    <label for="passwordRegister">Password</label>
-                    <input name="password" type="password" class="form-control" id="passwordRegister" placeholder="Password">
-                </div>
-
-                <div class="form-group">
-                    <label for="passwordRegister">Confirm password</label>
-                    <input name="password2" type="password" class="form-control" id="passwordRegister" placeholder="Confirm your password">
-                </div>
-
-                <div class="form-group">
-                    <label for="birthDateRegister">Birth date</label>
-                    <div class="input-group">
-                      <input class="form-control date" name="date" id="date" type="date" value="">
-                      <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+                    <div class="form-group">
+                        <input name="page" type="hidden" class="form-control" value ="register.php">
                     </div>
-                  </div>
 
-                <script type="text/javascript">
-                  $('#date').datetimepicker({
-                  todayBtn:"true",
-                  format:"dd/mm/yyyy", 
-                  autoclose:"true",
-                  pickerPosition:"bottom-left",
-                  minView:"month",
-                  language:"fr"
-                  });
-                </script>
-                
-                <button type="submit" class="btn btn-default">Register</button>
-            </form>
+                    <div class="form-group">
+                        <label for="nameRegister">Name</label>
+                        <input name="prenom" type="name" class="form-control" id="nameRegister" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="surnameRegister">Surname</label>
+                        <input name="nom" type="surname" class="form-control" id="surnameRegister" placeholder="Surname">
+                    </div>
+                    <div class="form-group">
+                        <label for="usernameRegister">Username</label>
+                        <input name="pseudo" type="username" class="form-control" id="usernameRegister" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <label for="emailRegister">Email address</label>
+                        <input name="email" type="email" class="form-control" id="emailRegister" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="passwordRegister">Password</label>
+                        <input name="password" type="password" class="form-control" id="passwordRegister" placeholder="Password">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="passwordRegister">Confirm password</label>
+                        <input name="password2" type="password" class="form-control" id="passwordRegister" placeholder="Confirm your password">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="birthDateRegister">Birth date</label>
+                        <div class="input-group">
+                            <input class="form-control date" name="date" id="date-register" type="date" value="">
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-default">Register</button>
+                </form>
             <?php else : ?>
                 <div class="well well-lg">There is no account left for now. Come back later!</div>
             <?php endif; ?>
