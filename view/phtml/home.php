135cf56df9595fb9e8120bcea36a1cea4e455a35
hundrex <kiruahxh32@yahoo.fr>
2016-06-15 15:21:57 +0200

    contenu graphique (si on veut) de home.php

--- view/phtml/home.php
+++ view/phtml/home.php
@@ -1,7 +1,75 @@
 <html>
     <body>
-        <div>
-            <p>La page d'accueil</p>
+        <div class="row">
+            <div class="col-lg-8 col-md-offset-2">
+                <img class="img-responsive" src="./view/document/picture/evolveFullLogo.png" alt="">
+                <br />
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-offset-3">
+            <h1><p><b>EV</b>olve <b>O</b>n <b>L</b>inux <b>V</b>irtualisation <b>E</b>asily</p></h1>
+        </div>
+        <div class="col-lg-6 col-md-offset-3">
+            <h2>
+                <p>
+                    Evolve : Free yourself from thinking about hardware limitations<br/>
+                    Linux : Use a powerful environment that you can customize<br/>
+                    Virtualisation : Be safe when working your way through your goal<br/>
+                    Easily : Do not have any worries about configuration, we do it all for you<br/>
+                </p>
+            </h2>
+        </div>
+        <div class="col-lg-6 col-md-offset-3">
+            <h1><p>What is it ?</p></h1>
+        </div>
+        <div class="col-lg-6 col-md-offset-3">
+            <h2>
+                <p>
+                    Evolve is a cloud computing service which provide customized Linux environment in less than a minute, easily deployed, shareable on-the-fly, and accessible and managed only through a simple web browser. Flexibility, evolutivity and fiability are the key words of our project.
+                    <br />
+                </p>
+            </h2>
+        </div>
+        <div class="col-lg-6 col-md-offset-3">
+            <h1><p>Our team</p></h1>
+        </div>
+        <div class="col-lg-6 col-md-offset-3">
+            <h2>
+                <p>
+                    We are 4 students in second years of l'École des Mines d’Alès of speciality Networking and System. 
+                </p>
+                <ul>
+                    <li>
+                        <p>
+                            Aurélie GUY: Backend developer and MySQL Analyst. 
+                            In charge of web server backend development made in PHP, and database creation for the EVOLVE web site.
+                            aurelie.guy@mines-ales.org
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Alexis BATTAGLI: Backend and Web-Services developer. 
+                            In charge of web server backend development made in PHP, and web services conception.
+                            alexis.battagli@mines-ales.org
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Amine BOUROUIBA: LXC Expert and Project Manager. 
+                            In charge of the administration of container. 
+                            amine.bourouiba@mines-ales.org
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Tommy VALETTE: Frontend developer and Guacamole expert. 
+                            In charge of web site frontend development made in javascript and PHP, and Guacamole deployment and configuration. 
+                            tommy.valette@mines-ales.org
+                        </p>
+                    </li>
+                    
+                </ul>
+            </h2>
         </div>
     </body>
 </html>
